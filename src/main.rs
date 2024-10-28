dc1174ba32dacfea235fd062d7baba4b5790636c
Richard Strnad <git@strnad.ch>
2024-10-28 15:14:53 +0100

    feat: use u8 instead of chars for the salt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use core::panic;
 use std::env::args;
 
-const SALT: &str = "dsfd;kfoA,.iyewrkldJKDHSUBsgvca69834ncxv9873254k;fg87";
+const SALT: [u8; 53] = [
+    100, 115, 102, 100, 59, 107, 102, 111, 65, 44, 46, 105, 121, 101, 119, 114, 107, 108, 100, 74,
+    75, 68, 72, 83, 85, 66, 115, 103, 118, 99, 97, 54, 57, 56, 51, 52, 110, 99, 120, 118, 57, 56,
+    55, 51, 50, 53, 52, 107, 59, 102, 103, 56, 55,
+];
 
 fn main() {
     let pw = args().skip(1).next();
@@ -28,7 +32,7 @@ fn decrypt(pw: &str) -> Result<String, String> {
     Ok(hex_pw
         .iter()
         .enumerate()
-        .map(|(i, &hex_char)| (SALT.as_bytes()[(i + index) % 53] ^ hex_char) as char)
+        .map(|(i, &hex_char)| (SALT[(i + index) % 53] ^ hex_char) as char)
         .collect())
 }
 
